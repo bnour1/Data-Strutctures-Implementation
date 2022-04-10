8723e7962b1152da127baed89abeba4dddc8cdc5
bnour1 <brunowashingtong@gmail.com>
2022-04-09 23:52:07 -0300

    Atividade 4 Concluida

--- lde/atividade4/questao2/LogicaRemocaoLivro.java
+++ lde/atividade4/questao2/LogicaRemocaoLivro.java
@@ -1,8 +1,16 @@
 package lde.atividade4.questao2;
 
+/*
+2 - Considere uma lista linear duplamente encadeada cujos nós armazenam objetos da classe
+Livro. Considere ainda que a referida lista NÃO ESTÁ ORDENADA e PODE CONTER elementos
+repetidos. Implemente uma função para remover um dado Livro da lista. A função deverá receber
+como parâmetro o Livro a ser removido. A função deverá retornar quantas ocorrências do Livro
+foram removidas da lista.
+*/
+
 import lde.atividade4.questao1.*;
 
-public class LogicaRemocaoLivroQ1 extends LDE {
+public class LogicaRemocaoLivro extends LDE {
 
        public int remover(LDE lista, String id) { // Logica usada para a questão 2 (LIVROS) da atividade4
         User temp = new User(id);
