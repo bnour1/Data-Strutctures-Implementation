927cce0b2714005c76f6f6c3b74f444f457a5722
PC LAB201 UNICAP <lab201@unicap.br>
2022-04-22 07:16:26 -0300

    forpratice

--- temptreino/App.java
+++ temptreino/App.java
@@ -26,6 +26,43 @@ public class App {
                 case 2:
                 teste.showList();
                 break;
+                case 3:
+                System.out.println("Insira o id do objeto");
+                int idd = input.nextInt();
+                Objeto rem = new Objeto(idd, "remover");
+                teste.remove(rem);
+                break;
+                case 4:
+                LDE lista1 = new LDE();
+                LDE lista2 = new LDE();
+                int umid; String umtype;
+                
+                Objeto ob = new Objeto(13, "13");
+                lista1.add(ob);
+                ob = new Objeto(12, "12");
+                lista2.add(ob);
+                ob = new Objeto(21, "21");
+                lista1.add(ob);
+                lista2.add(ob);
+                ob = new Objeto(27, "27");
+                lista1.add(ob);
+                ob = new Objeto(26,"26");
+                lista2.add(ob);
+                ob = new Objeto(48,"48");
+                lista1.add(ob);
+                lista2.add(ob);
+                ob= new Objeto(63, "63");
+                lista1.add(ob);
+                lista2.add(ob);
+                ob = new Objeto(95, "95");
+                lista1.add(ob);
+                lista2.add(ob);
+                LDE lista3 = new LDE();
+                lista3 = teste.intersecao(lista1,lista2);
+                lista1.showList();
+                lista2.showList();
+                lista3.showList();
+                break;
                 default:
                 break;
             }
@@ -38,6 +75,8 @@ public class App {
     static void menu(){
         System.out.println("Op 1 Adicionar (ORD)");
         System.out.println("Op 2 Mostrar lista");
+        System.out.println("Op 3 remover objeto");
+        System.out.println("Op 4 intersecao");
     }
     
 }
--- temptreino/LDE.java
+++ temptreino/LDE.java
@@ -14,26 +14,26 @@ public class LDE {
         }
     }
 
-    public void add(Objeto o){
+    public void add(Objeto o) {
         Node novo = new Node(o);
 
-        if(isEmpty()){
+        if (isEmpty()) {
             this.primeiro = novo;
             this.ultimo = novo;
             qtd++;
-        }else if(this.primeiro.getInfo().compareTo(o) > 0){
+        } else if (this.primeiro.getInfo().compareTo(o) > 0) {
             novo.setProx(this.primeiro);
             this.primeiro.setAnt(novo);
             this.primeiro = novo;
             qtd++;
-        }else if(this.ultimo.getInfo().compareTo(o) < 0){
+        } else if (this.ultimo.getInfo().compareTo(o) < 0) {
             novo.setAnt(this.ultimo);
             this.ultimo.setProx(novo);
             this.ultimo = novo;
-            qtd++;         
-        }else{
+            qtd++;
+        } else {
             Node aux = this.primeiro;
-            while(aux.getProx().getInfo().compareTo(o) < 0){
+            while (aux.getProx().getInfo().compareTo(o) < 0) {
                 aux = aux.getProx();
             }
             novo.setAnt(aux);
@@ -42,6 +42,68 @@ public class LDE {
             aux.setProx(novo);
             qtd++;
         }
+    }
+
+    public void remove(Objeto o){
+
+        if(isEmpty()){
+            System.out.println("Elemento ausente da lista");
+        }else if(this.primeiro.getInfo().compareTo(o) == 0){
+            Node aux = this.primeiro.getProx();
+
+            aux.setAnt(null);
+            this.primeiro = aux;
+            qtd--;
+        }else if(this.ultimo.getInfo().compareTo(o) == 0){
+            Node aux = this.ultimo.getAnt();
+
+            aux.setProx(null);
+            this.ultimo = aux;
+            qtd--;
+        }else{
+            Node aux = this.primeiro;
+
+            while(aux.getProx().getInfo().compareTo(o) != 0){
+                aux = aux.getProx();
+            }
+            Node auxprox = aux.getProx();
+            auxprox.getProx().setAnt(aux);
+            aux.setProx(auxprox.getProx());
+
+            qtd--;
+        }
+
+    }
+
+    public Node getPrimeiro() {
+        return primeiro;
+    }
+
+    public void setPrimeiro(Node primeiro) {
+        this.primeiro = primeiro;
+    }
+
+    public LDE intersecao(LDE l1, LDE l2){
+        LDE result = new LDE();
+        Node aux = l1.getPrimeiro();
+        Node aux1 = l2.getPrimeiro();
+
+        while(aux != null){
+            if(aux.getInfo().compareTo(aux1.getInfo()) == 0){
+                System.out.println("t4");
+                result.add(aux.getInfo());
+                aux = aux.getProx();
+                aux1 = l2.getPrimeiro();
+            }else if(aux.getInfo().compareTo(aux1.getInfo()) > 0){
+                System.out.println("t2");
+                aux1=aux1.getProx();
+            }else if(aux.getInfo().compareTo(aux1.getInfo()) < 0){
+                System.out.println("t3");
+                aux = aux.getProx();
+                aux1 = l2.getPrimeiro();
+            }
+        }
+        return result;
     }
 
     public void showList(){
@@ -55,5 +117,4 @@ public class LDE {
             aux = aux.getProx();
         }
     }
-
 }
