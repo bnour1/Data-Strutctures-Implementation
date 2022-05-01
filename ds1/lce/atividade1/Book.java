d4e72af1623ef11ffd585a75344690735ca56d1c
bnour1 <brunowashingtong@gmail.com>
2022-05-01 12:21:07 -0300

    Atividade 1

---
+++ ds1/lce/atividade1/Book.java
@@ -0,0 +1,55 @@
+package ds1.lce.atividade1;
+
+public class Book implements Comparable<Book>{
+
+    private String title;
+    private String author;
+    private String Description;
+
+    public Book(String title, String author, String Description) {
+        this.title = title;
+        this.author = author;
+        this.Description = Description;
+    }
+
+    public Book(String Title){
+        this.title = Title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    @Override
+    public int compareTo(Book b){
+        return this.getTitle().compareTo(b.getTitle());
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [Description=" + Description + ", author=" + author + ", title=" + title + "]";
+    }
+
+    
+    
+}
